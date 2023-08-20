2ada4aae7ac542c7ee16315312157153664b4c1c
m4icol <m4icol.r12@gmail.com>
2023-08-20 16:06:15 -0500

    v. Agosto commit
    create section "contenido promocional"
    create new files for optimized code css

--- indexSlider.js
+++ indexSlider.js
@@ -6,13 +6,29 @@ const swiper = new Swiper('.swiper', {
     slidesPerView: 1,
     spaceBetween: 300,
     autoplay: {
-        delay: 3000, 
+        delay: 2000, 
     },
 
-
-    // Navigation arrows
     navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
+    },
+});
+
+var promoSlider = new Swiper('.promo-slider', {
+    direction: 'horizontal',
+    loop: true,
+    autoplay: true,
+    slidesPerView: 1,
+    spaceBetween: 100,
+    autoplay: {
+        delay: 2500, 
+    },
+
+    breakpoints: {
+        1200: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        }
     },
 });
